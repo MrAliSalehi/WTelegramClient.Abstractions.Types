9a989d6056feeadd122ce3c72a53a3818fbbf0c8
Ali <alisalehi498@yahoo.com>
2024-01-15 20:54:42 +0330

    unnecessary `.ToList()` call

--- WTC.Abstractions.Types/ChatsExtensions.cs
+++ WTC.Abstractions.Types/ChatsExtensions.cs
@@ -7,13 +7,13 @@ public static class ChatsExtensions
     public static IEnumerable<Channel> Channels(this Messages_Chats mc, Func<Channel, bool>? predicate = null)
     {
         var ofType = mc.chats.Values.OfType<Channel>();
-        return predicate is null ? ofType.ToList() : ofType.Where(predicate).ToList();
+        return predicate is null ? ofType : ofType.Where(predicate);
     }
 
     public static IEnumerable<Chat> Chats(this Messages_Chats mc, Func<Chat, bool>? predicate = null)
     {
         var ofType = mc.chats.Values.OfType<Chat>();
-        return predicate is null ? ofType.ToList() : ofType.Where(predicate).ToList();
+        return predicate is null ? ofType : ofType.Where(predicate);
     }
 
     public static InputPeerChannel ToInputPeerChannel(this Channel ch) => new(ch.ID == 0 ? ch.id : -1, ch.access_hash);
