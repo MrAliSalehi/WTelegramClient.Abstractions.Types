e5feedfb81734b5f34eee3bea80bf4b0a405c119
Ali <alisalehi498@yahoo.com>
2024-01-15 20:15:31 +0330

    chats

--- WTC.Abstractions.Types/ChatsExtensions.cs
+++ WTC.Abstractions.Types/ChatsExtensions.cs
@@ -9,5 +9,19 @@ public static class ChatsExtensions
         var ofType = mc.chats.Values.OfType<Channel>();
         return predicate is null ? ofType.ToList() : ofType.Where(predicate).ToList();
     }
-    public static InputPeerChannel ToInputPeerChannel(this Channel ch) => new (ch.ID == 0 ? ch.id : -1, ch.access_hash);
+    public static IEnumerable<Chat> Chats(this Messages_Chats mc, Func<Chat, bool>? predicate = null)
+    {
+        var ofType = mc.chats.Values.OfType<Chat>();
+        return predicate is null ? ofType.ToList() : ofType.Where(predicate).ToList();
+    }
+    public static InputPeerChannel ToInputPeerChannel(this Channel ch) => new(ch.ID == 0 ? ch.id : -1, ch.access_hash);
+
+    public static InputPeerChat ToInputPeerChat(this Chat ch) => new(ch.ID == 0 ? ch.id : -1);
+
+    public static bool IsForbidden(this ChatBase b) => b switch
+    {
+        ChannelForbidden => true,
+        ChatForbidden    => true,
+        _                => false,
+    };
 }
